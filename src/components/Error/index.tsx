db5f0ff7a9d789e0481f7e3e9f64f52a44fcd0cc
FanishChandrakar <FanishChandrakar>
2021-10-04 00:40:32 +0530

    init

---
+++ src/components/Error/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { NextPage } from "next";
+import styles from "@/styles/Error.module.css";
+
+const NetworkStatus = require("./NetworkStatus.json");
+
+interface ErrorProps {
+    statusCode: number;
+}
+
+const Error: NextPage<ErrorProps> = ({ statusCode }) => {
+    return (
+        <div className={styles.container}>
+            <h1>{statusCode}</h1>
+            <p>Error: {NetworkStatus[statusCode]}</p>
+        </div>
+    );
+};
+
+Error.getInitialProps = ({ res, err }) => {
+    const currentStatusCode = res?.statusCode || 500;
+    const throwedStatusCode = err?.statusCode;
+
+    const statusCode = throwedStatusCode || currentStatusCode;
+
+    if (res) {
+        // Here is where the magic happens
+        res.statusCode = statusCode;
+    }
+
+    return { statusCode };
+};
+
+export default Error;
