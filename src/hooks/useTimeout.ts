3f4a7c75b40d7dea0e02fcddc2d45adebb217d07
FanishChandrakar <FanishChandrakar>
2021-11-06 18:16:33 +0530

    Hook Doc

--- src/hooks/useTimeout.ts
+++ src/hooks/useTimeout.ts
@@ -1,12 +1,24 @@
 import type { EffectCallback } from "react";
 import { useCallback, useEffect, useRef } from "react";
 
-export type UseTimeoutReturn = [reset: EffectCallback, clear: EffectCallback];
+export type UseTimeoutReturn = EffectCallback[];
 type UseTimeout = (
     callback: EffectCallback,
     delay: number | undefined
 ) => UseTimeoutReturn;
 
+/**
+ * ## useTimeout Hook
+ * Allows code execution after a specified amount of time.
+ * ```ts
+ *      const [reset, clear] = useTimeout(callback, delay);
+ * ```
+ * @param callback In case of timeout `callback` is executed.
+ * @param delay The `delay`, in milliseconds that the timer should wait before timeout.
+ * @returns `[ reset, clear ]`
+ * @method reset - Starts a new timer with `delay`.
+ * @method clear - Removes the timer.
+ */
 export const useTimeout: UseTimeout = (callback, delay) => {
     const callbackRef = useRef(callback);
     const timeoutRef = useRef<NodeJS.Timeout>();
@@ -19,6 +31,7 @@ export const useTimeout: UseTimeout = (callback, delay) => {
         timeoutRef.current = setTimeout(() => callbackRef.current(), delay);
     }, [delay]);
 
+    /** Remove the timer. */
     const clear = useCallback(() => {
         timeoutRef.current && clearTimeout(timeoutRef.current);
     }, []);
@@ -28,6 +41,7 @@ export const useTimeout: UseTimeout = (callback, delay) => {
         return clear;
     }, [delay, set, clear]);
 
+    /** Starts a new timer with `delay`. */
     const reset = useCallback(() => {
         clear();
         set();
